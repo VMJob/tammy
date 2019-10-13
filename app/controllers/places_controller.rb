7f397ea33cf793d61e47e1705841eca03efae80a
Vincent-Job <vinjob30@gmail.com>
2019-10-13 06:15:14 +0000

    update tammy

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   def index
     #Place.delete(Place.where(name:'Chinese Restaurant').last.id)
   	@places = Place.order(:id).paginate :page => params[:page], per_page: 3
@@ -21,10 +21,19 @@ class PlacesController < ApplicationController
   
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
     @place.update_attributes(place_params)
     redirect_to root_path
   end
