ba146403a56d676403ed993765fc746926ea27f4
Vincent-Job <vinjob30@gmail.com>
2019-10-15 22:55:11 +0000

    update tammy

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,18 @@
 class Place < ApplicationRecord
-   belongs_to :user
+  belongs_to :user
+  validates :address, presence: true
+  validates :description, prensence: true
+  validates :name, length: {minimum: 3}
+
    geocoded_by :address
    after_validation :geocode
-
-   validates :name, presence: true
 end
+
+
+
+  
+
+
+  
+
+
